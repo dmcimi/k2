580ddc80c5f5594a034c0a0d24dee7c1899ca125
小和 <61643603+dmcimi@users.noreply.github.com>
2021-02-08 01:15:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,23 +11,23 @@
 #=================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #=================================================
 # 清除默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #=================================================
 # 清除旧版argon主题并拉取最新版
 # 删除老argon主题
-#rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
 #=================================================
 # 拉取argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #=================================================
 # 增加插件包
-#git clone https://github.com/kenzok8/openwrt-packages.git package/lean/
+git clone https://github.com/kenzok8/openwrt-packages.git package/lean/
 #=================================================
 # 增加插件包依赖
-#git clone https://github.com/kenzok8/small.git package/lean/
+git clone https://github.com/kenzok8/small.git package/lean/
 #=================================================
 #增加插件包
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
