5b7d638b9e6c6b756406fd245289e57407217991
小和 <61643603+dmcimi@users.noreply.github.com>
2021-02-08 00:20:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,28 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
+#=================================================
+#1. Modify default IP
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+#=================================================
+# 清除默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#=================================================
+# 清除旧版argon主题并拉取最新版
+# 删除老argon主题
+rm -rf package/lean/luci-theme-argon
+#=================================================
+# 拉取argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#=================================================
+# 增加插件包
+#git clone https://github.com/kenzok8/openwrt-packages.git package/lean/
+#=================================================
+# 增加插件包依赖
+#git clone https://github.com/kenzok8/small.git package/lean/
+#=================================================
+#增加插件包
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+© 2021 GitHub, Inc.
