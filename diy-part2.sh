26d49ec5397c22ea54d728d5d12c98e7ed3c1998
小和 <61643603+dmcimi@users.noreply.github.com>
2021-02-08 00:35:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,7 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 #=================================================
 # 清除旧版argon主题并拉取最新版
 # 删除老argon主题
-rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
 #=================================================
 # 拉取argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
